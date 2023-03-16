b878bbc4a4e9358d63a26bd9a62615c5635a7bda
Matteo Picciolini <Matteo Picciolini>
2023-03-16 01:57:15 +0100

    New implemetation. HDR Image write_pfm and some new files and test

--- src/HDR_Image.java
+++ src/HDR_Image.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+
+import static java.nio.ByteOrder.LITTLE_ENDIAN;
 import static org.junit.Assert.assertTrue;
 public class HDR_Image {
     public int height;
@@ -33,4 +40,34 @@ public class HDR_Image {
         assertTrue(this.valid_coordinates(x, y));
         this.pixels[this.pixel_offset(x, y)] = color;
     }
+
+    public void write_pfm(OutputStream stream, ByteOrder order) throws IOException {
+        String endianness_str = (order == LITTLE_ENDIAN) ?  "-1.0" :  "1.0";
+        String header = "PF\n" + this.width + " " + this.height + "\n" + endianness_str + "\n";
+        byte [] bytes = header.getBytes(StandardCharsets.US_ASCII);
+        stream.write(bytes, 0, bytes.length);
+
+        Color color;
+        for (int i = this.height - 1; i >= 0 ; --i){
+            for(int j = 0; j < this.width; ++j){
+                color = this.get_pixel(j, i);
+                writeFloatToStream(stream, color.r, order);
+                writeFloatToStream(stream, color.g, order);
+                writeFloatToStream(stream, color.b, order);
+            }
+        }
+    }
+
+    private void writeFloatToStream(OutputStream stream, float value, ByteOrder order) throws IOException {
+        //each float have 4 bytes. I allocate 4 bytes and if I use Litte_Endian, I reverse these bytes
+        byte[] bytes = ByteBuffer.allocate(4).putFloat(value).array();
+        if (order == LITTLE_ENDIAN){
+            Functions_Constants.byte_reverse(bytes, 4);
+        }
+        stream.write(bytes);
+
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import java.io.*;
+import java.nio.ByteOrder;
+
 
 public class Main {
-    public static void main(String[] args) {
-        //making some tries of Color class
-        Color color1 = new Color(1.0f, 3.0f, 5.0f);
-        Color color2 = new Color(2.0f, 4.0f, 6.0f);
-        Color color3 = color1.sum(color2);
-        //System.out.println(color3.r);
+    public static void main(String[] args) throws IOException {
 
-        //Let's use the HDR class and build some pixels
-        int height = 1080;
-        int width = 1920;
-        HDR_Image hdr = new HDR_Image(width, height);
-        hdr.set_pixel(3, 7, color1);
-        System.out.println(hdr.get_pixel(3, 7).toString());
-        Color incr = new Color(1.0f, 1.0f, 1.0f);
-        for (int i = 0; i < 100; ++i) {
-            color1 = color1.sum(incr);
-            hdr.set_pixel(i, i, color1);
-            System.out.println(hdr.get_pixel(i, i).toString());
-        }
+
     }
 }
