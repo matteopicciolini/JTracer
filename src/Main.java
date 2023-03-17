63fd31fcf6e6f80cfc4a36f47c0130f8c982b812
Francesco Villa <fravilla30@gmail.com>
2023-03-17 10:49:29 +0100

    Reading_float funtion in the main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 import org.junit.jupiter.api.Assertions;
 import java.io.*;
+import java.util.*;
 import java.nio.ByteOrder;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        HDR_Image img=new HDR_Image(3, 2);
+        int i=0;
+        FileReader is = new FileReader("reference_be.pfm");
+        BufferedReader bf = new BufferedReader(is);
+        ByteArrayOutputStream bf2 = new ByteArrayOutputStream();
+        Vector v1=new Vector();
+
+        InputStream targetStream = new ByteArrayInputStream("Hello\nWorld".getBytes());
+        InputStream inputStream=new FileInputStream("reference_be.pfm");
+        while(bf.ready()){
+            System.out.println(bf.readLine());
+            bf2.write(bf.readLine().getBytes());
+            i++;
+        }
+        System.out.println(bf2);
+
 
 
     }
 }
+
