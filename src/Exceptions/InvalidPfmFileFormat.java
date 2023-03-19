0b96037eff7f72d336d7010c2ba94ff191abc2d3
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-03-19 01:54:15 +0100

    Exceptions updated

--- src/Exceptions/InvalidPfmFileFormat.java
+++ src/Exceptions/InvalidPfmFileFormat.java
@@ -1,7 +1,7 @@
 package Exceptions;
 
 public class InvalidPfmFileFormat extends Throwable {
-    public InvalidPfmFileFormat(String s) {
-        System.out.println(s);
+    public InvalidPfmFileFormat(String mex) {
+        super(mex);
     }
 }
