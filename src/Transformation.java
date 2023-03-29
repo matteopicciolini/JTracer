a27b153bc3966d0d34d6459475eb9d838156fde1
Francesco Villa <fravilla30@gmail.com>
2023-03-29 16:00:18 +0100

    Transf correction

--- src/Transformation.java
+++ src/Transformation.java
@@ -1,3 +1,14 @@
 public class Transformation {
+    public float[] m;
+    public float[] m_i;
 
+
+    public Transformation(){
+        m= new float[]{1f, 0f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 0f, 1f};
+        m_i=new float[]{1f, 0f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 0f, 1f};
+    }
+    public Transformation(float[] m){
+        this.m=m;
+        m_i=null;
+    }
 }
