57c74cd22a8f7db9e99084614f58fbd7172157bb
Francesco Villa <fravilla30@gmail.com>
2023-03-29 14:59:03 +0100

    First version of geometry classes

---
+++ src/Normal.java
@@ -0,0 +1,81 @@
+public class Normal {
+
+    public float x;
+    public float y;
+    public float z;
+
+    public Normal() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public Normal(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+    @Override
+    public String toString() {
+        return "<r: %s, g: %s, b: %s>".formatted(this.x, this.y, this.z);
+    }
+
+    public Normal difference(Normal normal1) {
+        Normal normal = new Normal(Math.abs(this.x - normal1.x), Math.abs(this.y - normal1.y), Math.abs(this.z - normal1.z));
+        return normal;
+    }
+
+    public boolean is_close(Normal normal1) {
+        float epsilon = 1e-5F;
+        Normal norm_diff = this.difference(normal1);
+        return (norm_diff.x < epsilon &&
+                norm_diff.y < epsilon &&
+                norm_diff.z < epsilon);
+    }
+
+    public Normal negation() {
+        Normal normal = new Normal(-this.x, -this.y, -this.z);
+        return normal;
+    }
+
+    public Normal dilatation(float scal) {
+        Normal normal = new Normal(this.x * scal, this.y * scal, this.z * scal);
+        return normal;
+    }
+    public float prod(Normal normal1) {
+        Normal normal = new Normal();
+        normal.x = this.x * normal1.x;
+        normal.y = this.y * normal1.y;
+        normal.z = this.z * normal1.z;
+        return normal.x+normal.y+normal.z;
+    }
+
+    public float norm() {
+
+        return (float) Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public float norm_sq(Normal normal1) {
+        return x * x + y * y + z * z;
+    }
+
+    public float scalar_prod(Normal normal1) {
+        float angle = prod(normal1);
+        return (this.norm() * normal1.norm())*angle ;
+    }
+    public Normal normalization() {
+        Normal norm=new Normal(x, y, z);
+        return norm.dilatation(1/norm.norm());
+    }
+    public Normal cross(Normal normal1) {
+                Normal normal2= new Normal(y * normal1.z - z * normal1.y, z * normal1.x - x * normal1.z, x* normal1.y - y * normal1.x);
+                return normal2;
+
+    }
+
+}
+
+
+
