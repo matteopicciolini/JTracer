6942bd0847307f7f13c1115ca5e093700382135b
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-04-08 03:16:54 +0200

    Implement Tests

--- src/test/java/org.mirrors/HDRImageTest.java
+++ src/test/java/org.mirrors/HDRImageTest.java
@@ -2,8 +2,6 @@ package org.mirrors;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
