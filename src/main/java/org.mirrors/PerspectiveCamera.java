28b810c099fa0a8aaca985be350de9be11f28868
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-04-19 16:08:45 +0200

    Implement Tests and fix bug in PerspectiveCamera

--- src/main/java/org.mirrors/PerspectiveCamera.java
+++ src/main/java/org.mirrors/PerspectiveCamera.java
@@ -1,15 +1,15 @@
 package org.mirrors;
 
-public class PerspectioveCamera extends Camera {
+public class PerspectiveCamera extends Camera {
     public float distance;
 
-    public PerspectioveCamera(float distance, float aspectRatio){
+    public PerspectiveCamera(float distance, float aspectRatio){
         this.distance = distance;
         this.aspectRatio = aspectRatio;
         this.transformation = new Transformation();
     }
 
-    public PerspectioveCamera(float distance, float aspectRatio, Transformation transformation){
+    public PerspectiveCamera(float distance, float aspectRatio, Transformation transformation){
         this.distance = distance;
         this.aspectRatio = aspectRatio;
         this.transformation = transformation;
@@ -17,6 +17,6 @@ public class PerspectioveCamera extends Camera {
     public Ray fireRay(float u, float v){
         Point origin = new Point(-this.distance, 0.f, 0.f);
         Vec direction = new Vec(this.distance, (1.f - 2.f * u) * this.aspectRatio, 2.f * v - 1.f);
-        return new Ray(origin, Global.VecX).transform(this.transformation);
+        return new Ray(origin, direction).transform(this.transformation);
     }
 }
