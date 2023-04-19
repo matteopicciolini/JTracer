7e83b721cc1bfe1ffd4567be5ab732cb31d0a621
Francesco Villa <fravilla30@gmail.com>
2023-04-19 12:04:52 +0100

    Create ray class

---
+++ src/main/java/org.mirrors/Ray.java
@@ -0,0 +1,36 @@
+package org.mirrors;
+
+public class Ray {
+    Point origin = new Point();
+    Vec dir = new Vec();
+    float tmin = 1e-5f;
+    float tmax = Float.POSITIVE_INFINITY;
+    int depth = 0;
+
+    public Ray(Point origin, Vec dir, int depth) {
+        this.origin=origin;
+        this.dir=dir;
+        this.depth=depth;
+    }
+
+    public Point at(float t) throws InvalidMatrixException {
+        Transformation trans=new Transformation();
+        return (Point) (trans.translation((Vec) this.dir.dot(t))).times(this.origin);
+    }
+
+
+    public boolean isClose(Ray ray1) {
+        boolean b1 = ray1.origin.isClose(this.origin);
+        boolean b2 = ray1.dir.isClose(this.dir);
+        if (b1 == true && b2 == true) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    public Ray transform(Transformation trans){
+        Ray ray=new Ray((Point) trans.times(this.origin), (Vec) trans.times(this.dir), this.depth);
+        return ray;
+    }
+}
