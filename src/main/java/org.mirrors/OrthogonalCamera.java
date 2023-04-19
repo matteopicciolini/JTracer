bfe7d7428ea131dc47c0e4909c21f38b06b1cd47
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-04-19 12:51:39 +0200

    Implement fireRay in Camera subclasses

--- src/main/java/org.mirrors/OrthogonalCamera.java
+++ src/main/java/org.mirrors/OrthogonalCamera.java
@@ -1,8 +1,17 @@
 package org.mirrors;
 
 public class OrthogonalCamera extends Camera {
-    public void fireRay(float u, float v){
+    public OrthogonalCamera(float aspectRatio){
+        this.aspectRatio = aspectRatio;
+        this.transformation = new Transformation();
+    }
+
+    public OrthogonalCamera(float aspectRatio, Transformation transformation){
+        this.aspectRatio = aspectRatio;
+        this.transformation = transformation;
+    }
+    public Ray fireRay(float u, float v){
         Point origin = new Point(-1.f, (1.f - 2.f * u) * this.aspectRatio, 2.f * v - 1.f);
-        return new Ray(origin, direction, 1.0f).transform(this.transformation);
+        return new Ray(origin, Global.VecX).transform(this.transformation);
     }
 }
--- src/main/java/org.mirrors/PerspectioveCamera.java
+++ src/main/java/org.mirrors/PerspectioveCamera.java
@@ -1,4 +1,22 @@
 package org.mirrors;
 
 public class PerspectioveCamera extends Camera {
+    public float distance;
+
+    public PerspectioveCamera(float distance, float aspectRatio){
+        this.distance = distance;
+        this.aspectRatio = aspectRatio;
+        this.transformation = new Transformation();
+    }
+
+    public PerspectioveCamera(float distance, float aspectRatio, Transformation transformation){
+        this.distance = distance;
+        this.aspectRatio = aspectRatio;
+        this.transformation = transformation;
+    }
+    public Ray fireRay(float u, float v){
+        Point origin = new Point(-this.distance, 0.f, 0.f);
+        Vec direction = new Vec(this.distance, (1.f - 2.f * u) * this.aspectRatio, 2.f * v - 1.f);
+        return new Ray(origin, Global.VecX).transform(this.transformation);
+    }
 }
