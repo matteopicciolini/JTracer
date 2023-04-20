cdf4d9b71e78d1586554caf731afe1f6110bfed6
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-04-20 16:22:59 +0200

    Add comments

--- src/main/java/org.mirrors/OrthogonalCamera.java
+++ src/main/java/org.mirrors/OrthogonalCamera.java
@@ -1,15 +1,36 @@
 package org.mirrors;
 
+/**
+ * A camera that creates orthogonal rays for rendering a scene.
+ */
 public class OrthogonalCamera extends Camera {
+    /**
+     * Creates a new orthogonal camera with the specified aspect ratio.
+     * @param aspectRatio the aspect ratio of the camera's view
+     */
     public OrthogonalCamera(float aspectRatio){
         this.aspectRatio = aspectRatio;
         this.transformation = new Transformation();
     }
 
+    /**
+     * Creates a new orthogonal camera with the specified aspect ratio and transformation.
+     * @param aspectRatio the aspect ratio of the camera's view
+     * @param transformation the transformation to apply to the camera
+     */
     public OrthogonalCamera(float aspectRatio, Transformation transformation){
         this.aspectRatio = aspectRatio;
         this.transformation = transformation;
     }
+
+    /**
+     * Fires an orthogonal ray from the camera's position through the specified
+     * pixel coordinates.
+     * @param u the horizontal coordinate of the pixel, in the range [-1, 1]
+     * @param v the vertical coordinate of the pixel, in the range [-1, 1]
+     * @return the ray originating from the camera's position and passing through
+     *         the specified pixel coordinates
+     */
     public Ray fireRay(float u, float v){
         Point origin = new Point(-1.f, (1.f - 2.f * u) * this.aspectRatio, 2.f * v - 1.f);
         return new Ray(origin, Global.VecX).transform(this.transformation);
--- src/main/java/org.mirrors/Ray.java
+++ src/main/java/org.mirrors/Ray.java
@@ -1,6 +1,9 @@
 package org.mirrors;
 import static org.mirrors.Transformation.translation;
 
+/**
+ A class representing a ray in 3D space.
+ */
 public class Ray {
     Point origin = new Point();
     Vec dir = new Vec();
@@ -8,27 +11,60 @@ public class Ray {
     float tMax = Float.POSITIVE_INFINITY;
     int depth = 0;
 
+    /**
+     Default constructor for Ray class.
+     */
     public Ray() {}
+
+    /**
+     Constructor for Ray class.
+     @param origin the origin point of the ray.
+     @param dir the direction vector of the ray.
+     @param depth the depth of the ray for tracing rays recursively.
+     */
     public Ray(Point origin, Vec dir, int depth) {
         this.origin = origin;
         this.dir = dir;
         this.depth = depth;
     }
+
+    /**
+     Constructor for Ray class.
+     @param origin the origin point of the ray.
+     @param dir the direction vector of the ray.
+     */
     public Ray(Point origin, Vec dir) {
         this.origin = origin;
         this.dir = dir;
     }
 
+    /**
+     Calculate the point on the ray at distance t from the origin.
+     @param t the distance from the origin point of the ray.
+     @return the point on the ray at distance t from the origin.
+     @throws InvalidMatrixException if the matrix is not valid for the operation.
+     */
     public Point at(float t) throws InvalidMatrixException {
         return (Point) (translation((Vec) this.dir.dot(t))).times(this.origin);
     }
 
-    public boolean isClose(Ray ray1) {
-        boolean b1 = ray1.origin.isClose(this.origin);
-        boolean b2 = ray1.dir.isClose(this.dir);
+    /**
+     Check if two rays are close to each other.
+     @param ray the ray to compare with this ray.
+     @return true if the two rays have close origin points and direction vectors.
+     */
+    public boolean isClose(Ray ray) {
+        boolean b1 = ray.origin.isClose(this.origin);
+        boolean b2 = ray.dir.isClose(this.dir);
         return b1 && b2;
     }
-    public Ray transform(Transformation trans){
-        return new Ray((Point) trans.times(this.origin), (Vec) trans.times(this.dir), this.depth);
+
+    /**
+     Transform the ray with a given transformation.
+     @param transformation the transformation to apply to the ray.
+     @return the transformed ray.
+     */
+    public Ray transform(Transformation transformation){
+        return new Ray((Point) transformation.times(this.origin), (Vec) transformation.times(this.dir), this.depth);
     }
 }
