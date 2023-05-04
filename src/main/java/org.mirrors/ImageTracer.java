0dcf5feeb16d637639ffe055f6ccb950c114fb03
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-05-04 17:02:11 +0200

    Fix bug [issue #4](https://github.com/matteopicciolini/ray_tracing/issues/4)

--- src/main/java/org.mirrors/ImageTracer.java
+++ src/main/java/org.mirrors/ImageTracer.java
@@ -27,8 +27,8 @@ public class ImageTracer {
      @return the ray that passes through the specified pixel.
      */
     public Ray fire_ray(int col, int row, float u_pixel, float v_pixel){
-        float u = (col + u_pixel) / (this.image.width - 1);
-        float v = (row + v_pixel) / (this.image.height - 1);
+        float u = (col + u_pixel) / this.image.width;
+        float v = 1.f - (row + v_pixel) / this.image.height;
         return this.camera.fireRay(u, v);
     }
 
