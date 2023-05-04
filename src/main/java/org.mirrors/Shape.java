e5fa9a59822a52931efb875602b7973cd1bc4613
Francesco Villa <fravilla30@gmail.com>
2023-05-04 17:53:28 +0100

    Implement Shape class

---
+++ src/main/java/org.mirrors/Shape.java
@@ -0,0 +1,12 @@
+package org.mirrors;
+
+public abstract class Shape {
+    public Transformation tras;
+    public Shape(Transformation tras){
+        this.tras=tras;
+    }
+    public void ray_intersection(){
+
+    }
+
+}
