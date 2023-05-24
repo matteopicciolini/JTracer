cba0f81a828e89e774b1ef4e4e0383670ca7ec6e
Francesco Villa <fravilla30@gmail.com>
2023-05-24 15:58:40 +0100

    Diffuse BRDF

--- src/main/java/org.mirrors/DiffuseBRDF.java
+++ src/main/java/org.mirrors/DiffuseBRDF.java
@@ -17,4 +17,18 @@ public class DiffuseBRDF extends BRDF {
     public Color eval(Normal norm, Vec dir, Vec2d uv){
         return this.pigment.getColor(uv).prod((float) (this.reflectance / Math.PI));
     }
+
+    public Ray scatter_ray(PCG pcg, Vec incomingDir, Point interactionPoint,  Normal normal, int depth){
+        ONB onb = new ONB(normal);
+        float cos_theta_sq = pcg.random_float();
+        float cos_theta= (float) Math.sqrt(cos_theta_sq);
+        float sin_theta = (float) Math.sqrt(1.0 - cos_theta_sq);
+        float phi = (float) (2.0 * Math.PI * pcg.random_float());
+
+        return Ray(interactionPoint,
+                1 * Math.cos(phi) * cos_theta + onb.e2 * Math.sin(phi) * cos_theta + onb.e3 * sin_theta,
+                1.0e-3,
+                Float.POSITIVE_INFINITY,
+                depth);
+}
 }
