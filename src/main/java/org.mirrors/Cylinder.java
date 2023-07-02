04c2e8dacdae8e71cb090fd5c113dba2269ae322
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-07-02 20:22:01 +0200

    fix cylinder bug

--- src/main/java/org.mirrors/Cylinder.java
+++ src/main/java/org.mirrors/Cylinder.java
@@ -22,12 +22,71 @@ public class Cylinder extends Shape {
         super(transformation, material);
     }
 
+    @Override
+    public List<HitRecord> rayIntersectionList(Ray ray) {
+        Ray invRay = ray.transform(this.transformation.inverse());
+
+        float a = invRay.dir.x * invRay.dir.x + invRay.dir.y * invRay.dir.y;
+        float b = 2.f * (invRay.dir.x * invRay.origin.x + invRay.dir.y * invRay.origin.y);
+        float c = invRay.origin.x * invRay.origin.x + invRay.origin.y * invRay.origin.y - 1.f;
+
+        float delta = b * b - 4.f * a * c;
+        if (delta <= 0.0f) return null;
+        float sqrtDelta = (float) sqrt(delta);
+        float tMin = (-b - sqrtDelta) / (2.0f * a);
+        float tMax = (-b + sqrtDelta) / (2.0f * a);
+
+        List<HitRecord> intersections = new ArrayList<>();
+
+        Point hitPoint1 = invRay.at(tMin);
+        float phi1 = (float) Math.atan2(hitPoint1.y, hitPoint1.x);
+        if (phi1 < 0) phi1 += 2.0f * (float) Math.PI;
+        if (tMin < invRay.tMax && tMin > invRay.tMin && (hitPoint1.z > 0.0f && hitPoint1.z < 1.0f)) {
+            float u = phi1 / (2.0f * (float) Math.PI);
+            float v = hitPoint1.z;
+            Normal normal = new Normal(hitPoint1.x, hitPoint1.y, 0.0f);
+            if (normal.toVec().dot(invRay.dir) > 0.0f) normal = (Normal) normal.neg();
+            intersections.add(new HitRecord(
+                    (Point) this.transformation.times(hitPoint1),
+                    (Normal) this.transformation.times(normal),
+                    new Vec2d(u, v),
+                    tMin,
+                    ray,
+                    this));
+        }
+        Point hitPoint2 = invRay.at(tMin);
+        float phi2 = (float) Math.atan2(hitPoint2.y, hitPoint2.x);
+        if (phi2 < 0) phi2 += 2.0f * (float) Math.PI;
+        if (tMax < invRay.tMax && tMax > invRay.tMin && (hitPoint2.z > 0.0f && hitPoint2.z < 1.0f)) {
+            float u = phi2 / (2.0f * (float) Math.PI);
+            float v = hitPoint2.z;
+            Normal normal = new Normal(hitPoint2.x, hitPoint2.y, 0.0f);
+            if (normal.toVec().dot(invRay.dir) > 0.0f) normal = (Normal) normal.neg();
+            intersections.add(new HitRecord(
+                    (Point) this.transformation.times(hitPoint2),
+                    (Normal) this.transformation.times(normal),
+                    new Vec2d(u, v),
+                    tMax,
+                    ray,
+                    this));
+        }
+
+        return intersections.isEmpty() ? null : intersections;
+    }
+
+    @Override
+    public boolean isInternal(Point point) {
+        point = (Point) this.transformation.inverse().times(point);
+        float dist = point.x * point.x + point.y * point.y;
+        return dist < 1.0f && (point.z > 0.0f && point.z < 1.0f);
+    }
+
+
+
+
 
     @Override
     public HitRecord rayIntersection(Ray ray) {
-        List<HitRecord> rayIntersectionList = this.rayIntersectionList(ray);
-        return rayIntersectionList == null ? null : rayIntersectionList.get(0);
-        /*
         Ray invRay = ray.transform(this.transformation.inverse());
         float a = invRay.dir.x * invRay.dir.x + invRay.dir.y * invRay.dir.y;
         float b = 2.f * (invRay.dir.x * invRay.origin.x + invRay.dir.y * invRay.origin.y);
@@ -67,9 +126,12 @@ public class Cylinder extends Shape {
                 firstHitT,
                 ray,
                 this
-        );*/
+        );
     }
 
+
+
+    /*
     @Override
     public List<HitRecord> rayIntersectionList(Ray ray) {
         Ray invRay = ray.transform(this.transformation.inverse());
@@ -281,6 +343,6 @@ public class Cylinder extends Shape {
     private Normal getNormal(Point p, Vec rayDir) {
         Normal n = new Normal(p.x, p.y, 0.0f);
         return (p.x * rayDir.x + p.y * rayDir.y < 0.0f) ? n : (Normal) n.neg();
-    }
+    }*/
 
 }
