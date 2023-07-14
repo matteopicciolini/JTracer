365a013793f6eb1cdc6e8193069d979094f39fe0
Matteo Picciolini <picciolinimatteo@gmail.com>
2023-07-15 01:56:12 +0200

    correct bug in sum

--- parallel-generate-image.sh
+++ parallel-generate-image.sh
@@ -8,8 +8,8 @@ fi
 readonly angle="$1"
 readonly angleNNN=$(printf "%03d" $angle)
 readonly pfmfile=image$angleNNN.pfm
-readonly pngfile=image$angleNNN.png
+readonly pngfile=cornell_$angleNNN.png
 
-time ./gradlew run --args="-d 500 500 $angle false $pfmfile pathTracer true false 1"
-
+#time ./gradlew run --args="-d 500 500 $angle false $pfmfile pathTracer true false 1"
+time ./gradlew run --args="render -a=0 --output=$pfmfile --input=firstImage.txt -n=10 --antialiasing=true -w=500 --height=500 --initSeq=$angle --initState=$angle"
 #parallel -j NUM_OF_CORES ./generate-image.sh '{}' ::: $(seq 0 359)
