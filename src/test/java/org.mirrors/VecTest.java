2f43c75b365a5772dc0da322db0fd1c262815310
Francesco Villa <fravilla30@gmail.com>
2023-07-17 19:30:34 +0100

    fix tests

--- src/test/java/org.mirrors/VecTest.java
+++ src/test/java/org.mirrors/VecTest.java
@@ -15,7 +15,7 @@ class VecTest {
         assertTrue(a.sum(b).isClose(new Vec(5.0f, 8.0f, 11.0f)));
         assertTrue(b.minus(a).isClose(new Vec(3.0f, 4.0f, 5.0f)));
         assertTrue(a.dot(2.0f).isClose(new Vec(2.0f, 4.0f, 6.0f)));
-        assertEquals(a.dot(b, Vec.class), 40.0f, 1e-5);
+        assertEquals(a.dot(b), 40.0f, 1e-5);
         assertTrue(a.cross(b).isClose(new Vec(-2.0f, 4.0f, -2.0f)));
         assertTrue(b.cross(a).isClose(new Vec(2.0f, -4.0f, 2.0f)));
         assertEquals(a.squaredNorm(), 14.0f, 1e-5);
